385d5c906d52d1479fb68a93efbdac1f8556ea86
sinbadBahri <h.l.kh7990@gmail.com>
2024-08-18 14:48:21 +0330

    The Authentication has changed from session to JWT. When a user sends a valid login credentials as a request, a Json Web Token will be created which would be saved as Cookie.
     Changes to be committed:
    	modified:   app/Http/Controllers/Auth/AuthenticatedSessionController.php
    	modified:   app/Models/User.php
    	modified:   config/auth.php

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\View\View;
 
 class AuthenticatedSessionController extends Controller
@@ -20,28 +21,62 @@ class AuthenticatedSessionController extends Controller
     }
 
     /**
-     * Handle an incoming authentication request.
+     * Handles the incoming authentication request.
+     * 
+     * If the credentials were correct, a Json Web Token would be created.
+     * This Token then would be saved as a Cookie.
+     * 
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)#: RedirectResponse
     {
-        $request->authenticate();
 
-        $request->session()->regenerate();
+        $credentials = request(['email', 'password']);
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        if (! $token = auth('web')->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        // dd($this->respondWithToken($token));
+
+        # Set the token as a cookie
+        return redirect()->intended(route('dashboard'))->cookie(
+        'token', # Cookie name
+        $token,  # Token value
+        auth('web')->factory()->getTTL() * 60, # Expiration in minutes
+        '/',    # Path
+        null,   # Domain (null for default)
+        true,   # Secure (true for HTTPS)
+        true    # HttpOnly
+        );
     }
 
     /**
-     * Destroy an authenticated session.
+     * Destroy an authenticated Cookie and erase the Bearer Token.
      */
     public function destroy(Request $request): RedirectResponse
     {
         Auth::guard('web')->logout();
 
-        $request->session()->invalidate();
+        $cookie = Cookie::forget('token');
 
-        $request->session()->regenerateToken();
-
-        return redirect('/');
+        return redirect('/')->withCookie($cookie);
     }
+
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('web')->factory()->getTTL() * 60
+        ]);
+    }
+
 }
